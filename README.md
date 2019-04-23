ef1bae91e2a6b80e92381f4cb5dafac470bbbeef
mdelpino <marc.delpino.sac@gmail.com>
2019-04-23 13:36:21 +0200

    README modified

--- README.md
+++ README.md
@@ -1,19 +1,90 @@
-# nucleosome_dynamics_singularity
+Nucleosome Dynamics - Singularity
+===================
+Nucleosome Dynamics Singularity image is a containerized implementation of 'Nucleosome Dynamics suite'.
 
-`singularity pull shub://nucleosome-dynamics/nucleosome_dynamics_singularity`
+#### Nucleosome Dynamics
+Nucleosome Dynamics is a suite of R programs for **nucleosome-related analyses** based on MNase-seq experimental data. The toolkit includes the following analyses:
+
+- nucleR: define and classify the location of nucleosomes from MNase-seq data.
+- Nucleosome Dynamics: compares different MNase-seq experiments identifying variations in nucleosomes location.
+- NFR: locate nucleosome-free regions
+- Periodicity: predicts  nucleosome phasing at gene level
+- TSS: classify transcription start sites based on the surrounding nucleosomes
+- Stiffness: computes nucleosome stiffness derived from a Gaussian function fitting
+
+#### Container software 
+This singularity image extends and distributes the following rellevant software:
+
+* R libraries:
+   * NucleR R library: https://github.com/nucleosome-dynamics/nucleR
+   * Nucleosome Dynamics R library: https://github.com/nucleosome-dynamics/NucDyn
+* R programs:
+   * Nucleosome Dynamics analyses: https://github.com/nucleosome-dynamics/nucleosome_dynamics
+
+# Running Nucleosome Dynamics
+Before using Singularity, you've to install singularity, if not previously done, you can do it by following the instructions from [Singularity install] (https://singularity.lbl.gov/docs-installation#install-the-master-branch) 
+
+The Singularity image for Nucleosome Dynamics can be found at [singularity hub](https://singularity-hub.org/collections/2579) as `nucleosome-dynamics/nucleosome_dynamics_singularity`. The image can be pulled by following the next command.
+```sh
+singularity pull shub://nucleosome-dynamics/nucleosome_dynamics_singularity
+```
+### Running an individual analysis
+You can run manually your containers using the following command:
+
+```sh
+singularity run --bind /path/to/data_dir/:/path/to/data_dir nucleosome_dynamics_singularity_latest.sif   [analysis] [analysis_options]
+```
+
+A short description of the parameters would be:
+- `singularity run` will run the container for you.
+- `--bind /path/to/data_dir/:/path/to/data_dir` will make the `data_dir` where input files are stored available to the container
+- `nucleosome_dynamics_singularity_latest.sif` is the image name, which can be found at [singularity hub] (https://singularity-hub.org/collections/2579).
+- `[analysis] [analysis_options]` correspond to the analysis type you want to run and its paremeters.
+
+Available analysis are:
+
+| `[analysis]` | Description |
+| -------- | -------- |
+| readBAM         | Read Aligned MSase-seq BAM into a RData structure (required for further process) |
+| nucleR           | Determine the positions of the nucleosomes across the genome |
+| NFR             | Short regions depleted of nucleosomes |
+| txstart         | Classify Transciption start accordint to the properties of surrounding nucleosomes  |
+| periodicity  | Periodic properties of nucleosomes inside gene bodies |
+| stiffness | Aparent stiffness constant foreach nucleosome obtained by fitting the coverage to a gaussian distribution |
+| nucleR_stats|  Nucleosome call statistics|
+| nucDyn          | Comparison of two diferent MNase-seq experiments to nucleosome architecture local changes |
+| NFR_stats|             Nucleosome Free Regions statistics|
+| txstart_stats|         TSS and TTS statistics|
+| periodicity_stats|  Statistics on Nucleosome periodicity|
+| stiffness_stats|      Statistics on stiffness|
+| nucDyn_stats|    Statistics on Nucleosome Dynamics analysis|
 
 
-`singularity exec --bind /galaxy_data/nuclDyn_data/tests/:/galaxy_data/nuclDyn_data/tests nucleosome_dynamics_singularity_latest.sif  /home/NucleosomeDynamics/runNuclDyn   readBAM --input /galaxy_data/nuclDyn_data/tests/sample-data/cellcycleG2_chrII.bam --output /galaxy_data/nuclDyn_data/tests/sample-data/dataset_readBAM545858_galaxy.RData --type paired `
-===================================================
-===> Nucleosome Dynamics (docker version) v0.1 <===
-===================================================
+Each analysis has its own input files and arguments. `singularity run nucleosome_dynamics_singularity_latest.sif [analysis] --help` will display such information in detail. Yet, a full usage description can be found at the [Nucleosome Dynamics repository](https://github.com/nucleosome-dynamics/nucleosome_dynamics).
 
-===> Running readBAM <===
+#### Example
 
--- loading /galaxy_data/nuclDyn_data/tests/sample-data/cellcycleG2_chrII.bam
+Here is an example on how to load a MNase-seq reads file using `readBAM`. It takes as input a BAM file and convert it into an RData file ready to be feed to other analyses ('nucleR', 'NFR', etc.).
 
-reading file /galaxy_data/nuclDyn_data/tests/sample-data/cellcycleG2_chrII.bam
-processing flags
-processing strand +
-processing strand -
--- saving /galaxy_data/nuclDyn_data/tests/sample-data/dataset_readBAM545858_galaxy.RData
+```sh
+singularity run --bind $PWD/test/data/:$PWD/test/data nucleosome_dynamics_singularity_latest.sif readBAM --input $PWD/test/data/cellcycleG2_chrII.bam --output $PWD/test/data/cellcycleG2_chrII.RData --type paired
+```
+
+# Running a workflow script
+You can combine different analysis tools to build your own workflow, you can do it following this command:
+
+```sh
+
+singularity run --bind /path/to/data_dir/:/path/to/data_dir nucleosome_dynamics_singularity_latest.sif  run  [WF_file_path]
+
+```
+Where:
+- `WF_file_path`: is a bash file containing the Rscript calls to different analyses.
+
+#### Example
+
+Here is an example on how to run the test workflow file `test/scripts/wf-test.sh`, a bash file sequencially calling all 'Nucleosome Dynamics' analyses.
+
+```
+singularity run --bind $PWD/test/data:$PWD/test/data nucleosome_dynamics_singularity_latest.sif  run $PWD/test/scripts/wf-test.sh 
+```
---
+++ test/scripts/wf-test.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+#  Copyright 2019 Barcelona Supercomputing Center
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+  
+
+#### SET CONTAINER  PATHS
+NUCLDYN=/home/NucleosomeDynamics/NucleosomeDynamics # NucDyn installation path inside container - Do not change
+SCRIPTS_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+VOLUME=$SCRIPTS_DIR/.. # Test data dir
+#NUCLDYN=$SCRIPTS_DIR/../../NucleosomeDynamics # NucDyn installation path
+
+echo '#running readBAM (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/readBAM.R --input $VOLUME/data/cellcycleG2_chrII.bam --output $VOLUME/data/outputs/cellcycleG2_chrII.RData --type paired 
+echo '#=============================================================='
+
+echo '#running readBAM (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/readBAM.R --input $VOLUME/data/cellcycleM_chrII.bam --output $VOLUME/data/outputs/cellcycleM_chrII.RData --type paired
+echo '#=============================================================='
+
+echo '#running nucleR (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/nucleR.R --input $VOLUME/data/outputs/cellcycleG2_chrII.RData --output $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --type paired --width 147 --minoverlap 80 --dyad_length 50 --thresholdPercentage 35 --hthresh 0.4 --wthresh 0.6 --pcKeepComp 0.02
+echo '#=============================================================='
+echo '#running nucleR (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/nucleR.R --input $VOLUME/data/outputs/cellcycleM_chrII.RData --output $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --type paired --width 147 --minoverlap 80 --dyad_length 50 --thresholdPercentage 35 --hthresh 0.4 --wthresh 0.6 --pcKeepComp 0.02
+echo '#=============================================================='
+echo '#running nucleR (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/nucleR.R --input $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --out_genes $VOLUME/data/outputs/NR_cellcycleG2_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/NR_cellcycleG2_chrII_stats.csv --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff
+echo '#=============================================================='
+echo '#running nucleR (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/nucleR.R --input $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --out_genes $VOLUME/data/outputs/NR_cellcycleM_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/NR_cellcycleM_chrII_stats.csv --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff
+echo '#=============================================================='
+echo '#running NFR (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/NFR.R --input $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --output $VOLUME/data/outputs/NFR_cellcycleG2_chrII.gff --minwidth 110 --threshold 400
+echo '#=============================================================='
+echo '#running NFR (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/NFR.R --input $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --output $VOLUME/data/outputs/NFR_cellcycleM_chrII.gff --minwidth 110 --threshold 400
+echo '#=============================================================='
+echo '#running NFR (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/NFR.R --input $VOLUME/data/outputs/NFR_cellcycleG2_chrII.gff --out_gw $VOLUME/data/outputs/NFR_cellcycleG2_chrII_stats.csv --genome $VOLUME/data/refGenomes//R64-1-1/genes.gff
+echo '#=============================================================='
+echo '#running NFR (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/NFR.R --input $VOLUME/data/outputs/NFR_cellcycleM_chrII.gff --out_gw $VOLUME/data/outputs/NFR_cellcycleM_chrII_stats.csv --genome $VOLUME/data/refGenomes//R64-1-1/genes.gff
+echo '#=============================================================='
+echo '#running txstart (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/txstart.R --calls $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --output $VOLUME/data/outputs/TSS_cellcycleG2_chrII.gff --window 300 --open_thresh 215
+echo '#=============================================================='
+echo '#running txstart (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/txstart.R --calls $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --output $VOLUME/data/outputs/TSS_cellcycleM_chrII.gff --window 300 --open_thresh 215
+echo '#=============================================================='
+echo '#running txstart (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/txstart.R --input $VOLUME/data/outputs/TSS_cellcycleG2_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/TSS_cellcycleG2_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/TSS_cellcycleG2_chrII_stats1.png --out_gw2 $VOLUME/data/outputs/TSS_cellcycleG2_chrII_stats2.png
+echo '#=============================================================='
+echo '#running txstart (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/txstart.R --input $VOLUME/data/outputs/TSS_cellcycleM_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/TSS_cellcycleM_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/TSS_cellcycleM_chrII_stats1.png --out_gw2 $VOLUME/data/outputs/TSS_cellcycleM_chrII_stats2.png
+echo '#=============================================================='
+echo '#running periodicity (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/periodicity.R --calls $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --reads $VOLUME/data/outputs/cellcycleG2_chrII.RData --type paired --gffOutput $VOLUME/data/outputs/P_cellcycleG2_chrII.gff --bwOutput $VOLUME/data/outputs/P_cellcycleG2_chrII.bw --genes $VOLUME/data/refGenomes//R64-1-1/genes.gff --chrom_sizes $VOLUME/data/refGenomes/R64-1-1/R64-1-1.fa.chrom.sizes --periodicity 165
+echo '#=============================================================='
+echo '#running periodicity (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/periodicity.R --calls $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --reads $VOLUME/data/outputs/cellcycleM_chrII.RData --type paired --gffOutput $VOLUME/data/outputs/P_cellcycleM_chrII.gff --bwOutput $VOLUME/data/outputs/P_cellcycleM_chrII.bw --genes $VOLUME/data/refGenomes//R64-1-1/genes.gff --chrom_sizes $VOLUME/data/refGenomes/R64-1-1/R64-1-1.fa.chrom.sizes --periodicity 165
+echo '#=============================================================='
+echo '#running periodicity (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/periodicity.R --input $VOLUME/data/outputs/P_cellcycleG2_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/P_cellcycleG2_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/P_cellcycleG2_chrII_stats.csv
+echo '#=============================================================='
+echo '#running periodicity (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/periodicity.R --input $VOLUME/data/outputs/P_cellcycleM_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/P_cellcycleM_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/P_cellcycleM_chrII_stats.csv
+echo '#=============================================================='
+echo '#running stiffness (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/stiffness.R --calls $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --reads $VOLUME/data/outputs/cellcycleG2_chrII.RData --output $VOLUME/data/outputs/STF_cellcycleG2_chrII.gff --range All
+echo '#=============================================================='
+echo '#running stiffness (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/stiffness.R --calls $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --reads $VOLUME/data/outputs/cellcycleM_chrII.RData --output $VOLUME/data/outputs/STF_cellcycleM_chrII.gff --range All
+echo '#=============================================================='
+echo '#running stiffness (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/stiffness.R --input $VOLUME/data/outputs/STF_cellcycleG2_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/STF_cellcycleG2_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/STF_cellcycleG2_chrII_stats1.csv --out_gw2 $VOLUME/data/outputs/STF_cellcycleG2_chrII_stats2.png
+echo '#=============================================================='
+echo '#running stiffness (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/stiffness.R --input $VOLUME/data/outputs/STF_cellcycleM_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/STF_cellcycleM_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/STF_cellcycleM_chrII_stats1.csv --out_gw2 $VOLUME/data/outputs/STF_cellcycleM_chrII_stats2.png
+echo '#=============================================================='
+echo '#running nucDyn (bin)'
+/usr/bin/Rscript $NUCLDYN/bin/nucDyn.R --input1 $VOLUME/data/outputs/cellcycleG2_chrII.RData --input2 $VOLUME/data/outputs/cellcycleM_chrII.RData --calls1 $VOLUME/data/outputs/NR_cellcycleG2_chrII.gff --calls2 $VOLUME/data/outputs/NR_cellcycleM_chrII.gff --outputGff $VOLUME/data/outputs/ND_cellcycleG2_chrII_cellcycleM_chrII.gff --outputBigWig $VOLUME/data/outputs/ND_cellcycleG2_chrII_cellcycleM_chrII.bw --plotRData $VOLUME/data/outputs/ND_cellcycleG2_chrII_cellcycleM_chrII_plot.RData --genome $VOLUME/data/refGenomes/R64-1-1/R64-1-1.fa.chrom.sizes --range All --maxDiff 70 --maxLen 140 --shift_min_nreads 3 --shift_threshold 0.1 --indel_min_nreads 3 --indel_threshold 0.05
+echo '#=============================================================='
+echo '#running nucDyn (statistics)'
+/usr/bin/Rscript $NUCLDYN/statistics/nucDyn.R --input $VOLUME/data/outputs/ND_cellcycleG2_chrII_cellcycleM_chrII.gff --genome $VOLUME/data/refGenomes/R64-1-1/genes.gff --out_genes $VOLUME/data/outputs/ND_cellcycleG2_chrII_cellcycleM_chrII_genes_stats.csv --out_gw $VOLUME/data/outputs/ND_cellcycleG2_chrII_cellcycleM_chrII_stats.png
+echo '#=============================================================='
